901af3c84541924c7c30ee29e88a610bfce03c67
Loren Gordon <loren@fleet-it.com>
2015-09-28 16:09:35 -0400

    Standardize on a 4-space indent

--- scripts/MakeGuac.sh
+++ scripts/MakeGuac.sh
@@ -36,36 +36,38 @@ MODUSER="/usr/sbin/usermod"
 # Create our SSH login-user
 if [[ $(${ADDUSER} ${SSHUSER})$? -ne 0 ]]
 then
-   (
-      printf "Failed to create ssh user account "
-      printf "[${SSHUSER}]. Aborting...\n"
-   ) > /dev/stderr
-   exit 1
+    (
+        printf "Failed to create ssh user account "
+        printf "[${SSHUSER}]. Aborting...\n"
+    ) > /dev/stderr
+    exit 1
 fi
 
 # Set password for our SSH login-user
 if  [[ $(${MODUSER} -p "${PWCRYPT}" ${SSHUSER}) -ne 0 ]]
 then
-   (
-      printf "Failed to set password for ssh user account. "
-      printf "Aborting...\n"
-   ) > /dev/stderr
-   exit 1
+    (
+        printf "Failed to set password for ssh user account. "
+        printf "Aborting...\n"
+    ) > /dev/stderr
+    exit 1
 fi
 
 # Add SSH login-user to sudoers list
-printf "${SSHUSER}\tALL=(root)\tNOPASSWD:ALL\n" > /etc/sudoers.d/user_${SSHUSER}
+printf "${SSHUSER}\tALL=(root)\tNOPASSWD:ALL\n" > \
+    /etc/sudoers.d/user_${SSHUSER}
 if [[ $? -ne 0 ]]
 then
-   echo "Failed to add ${SSHUSER} to sudoers" > /dev/stderr
-   exit 1
+    echo "Failed to add ${SSHUSER} to sudoers" > /dev/stderr
+    exit 1
 fi
 
 # Install OS standard Tomcat and Apache
 yum install -y httpd tomcat6
 
 # Install EPEL repos
-yum install -y http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+yum install -y \
+    http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 
 # Install guacamole components from EPEL
 yum -y install guacd libguac-client-*
@@ -73,7 +75,7 @@ yum -y install guacd libguac-client-*
 # Enable Web components to start at next boot
 for SVC in httpd tomcat6 guacd
 do
-   chkconfig ${SVC} on
+    chkconfig ${SVC} on
 done
 
 # Can't put this earlier because "not installed yet"
@@ -81,70 +83,74 @@ GVERS=$(rpm -q guacd --qf '%{version}')
 
 # Download version-matched Guacamole client from project repo
 curl -s -L ${GBINSRC}/guacamole-${GVERS}.war/download \
-   -o /var/lib/tomcat6/webapps/guacamole.war
+    -o /var/lib/tomcat6/webapps/guacamole.war
 
 # Gotta make SELinux happy...
 if [[ $(getenforce) = "Enforcing" ]] || [[ $(getenforce) = "Permissive" ]]
 then
-   chcon -R --reference=/var/lib/tomcat6/webapps /var/lib/tomcat6/webapps/guacamole.war
-   if [[ $(getsebool httpd_can_network_relay | \
-           cut -d ">" -f 2 | sed 's/[ ]*//g') = "off" ]]
-   then
-      echo "Enable httpd-based proxying within SELinux"
-      setsebool -P httpd_can_network_relay=1
-   fi
+    chcon -R --reference=/var/lib/tomcat6/webapps \
+        /var/lib/tomcat6/webapps/guacamole.war
+    if [[ $(getsebool httpd_can_network_relay | \
+        cut -d ">" -f 2 | sed 's/[ ]*//g') = "off" ]]
+    then
+        echo "Enable httpd-based proxying within SELinux"
+        setsebool -P httpd_can_network_relay=1
+    fi
 fi
 
 # Create /etc/guacamole as necessary
 if [[ ! -d /etc/guacamole ]]
 then
-   if [[ $(mkdir -p /etc/guacamole)$? -ne 0 ]]
-   then
-      echo "Cannot populate /etc/guacamole" > /dev/stderr
-      exit 1
-   fi
+    if [[ $(mkdir -p /etc/guacamole)$? -ne 0 ]]
+    then
+        echo "Cannot populate /etc/guacamole" > /dev/stderr
+        exit 1
+    fi
 fi
 
 # Create basic config files in /etc/guacamole
 cd /etc/guacamole
-(  echo "# Hostname and port of guacamole proxy"
-   echo "guacd-hostname: localhost"
-   echo "guacd-port:     4822"
-   echo ""
-   echo "# Location to read extra .jar's from"
-   echo "lib:  /var/lib/tomcat6/webapps/guacamole/WEB-INF/classes"
-   echo ""
-   echo "# Authentication provider class"
-   echo "auth-provider: net.sourceforge.guacamole.net.basic.BasicFileAuthenticationProvider"
-   echo ""
-   echo "# Properties used by BasicFileAuthenticationProvider"
-   echo "basic-user-mapping: /etc/guacamole/user-mapping.xml"
+(
+    echo "# Hostname and port of guacamole proxy"
+    echo "guacd-hostname: localhost"
+    echo "guacd-port:     4822"
+    echo ""
+    echo "# Location to read extra .jar's from"
+    echo "lib:  /var/lib/tomcat6/webapps/guacamole/WEB-INF/classes"
+    echo ""
+    echo "# Authentication provider class"
+    echo "auth-provider: net.sourceforge.guacamole.net.basic.BasicFileAuthenticationProvider"
+    echo ""
+    echo "# Properties used by BasicFileAuthenticationProvider"
+    echo "basic-user-mapping: /etc/guacamole/user-mapping.xml"
 ) > /etc/guacamole/guacamole.properties
-(  printf "<user-mapping>\n"
-   printf "\t<!-- Per-user authentication and config information -->\n"
-   printf "\t<authorize username=\"admin\" password=\"PASSWORD\">\n"
-   printf "\t\t<protocol>ssh</protocol>\n"
-   printf "\t\t\t<param name=\"hostname\">localhost</param>\n"
-   printf "\t\t\t<param name=\"port\">22</param>\n"
-   printf "\t</authorize>\n"
-   printf "</user-mapping>\n"
+(
+    printf "<user-mapping>\n"
+    printf "\t<!-- Per-user authentication and config information -->\n"
+    printf "\t<authorize username=\"admin\" password=\"PASSWORD\">\n"
+    printf "\t\t<protocol>ssh</protocol>\n"
+    printf "\t\t\t<param name=\"hostname\">localhost</param>\n"
+    printf "\t\t\t<param name=\"port\">22</param>\n"
+    printf "\t</authorize>\n"
+    printf "</user-mapping>\n"
 ) > /etc/guacamole/user-mapping.xml
-(  printf "<configuration>\n"
-   printf "\n"
-   printf "\t<!-- Appender for debugging -->\n"
-   printf "\t<appender name=\"GUAC-DEBUG\" class=\"ch.qos.logback.core.FileAppender\">\n"
-   printf "\t\t<file>/var/log/tomcat6/Guacamole.log</file>\n"
-   printf "\t\t<encoder>\n"
-   printf "\t\t\t<pattern>%%d{HH:mm:ss.SSS} [%%thread] %%-5level %%logger{36} - %%msg%%n</pattern>\n"
-   printf "\t\t</encoder>\n"
-   printf "\t</appender>\n"
-   printf "\n"
-   printf "\t<!-- Log at DEBUG level -->\n"
-   printf "\t<root level=\"debug\">\n"
-   printf "\t\t<appender-ref ref=\"GUAC-DEBUG\"/>\n"
-   printf "\t</root>\n"
-   printf "\n"
-   printf "</configuration>\n"
+(
+    printf "<configuration>\n"
+    printf "\n"
+    printf "\t<!-- Appender for debugging -->\n"
+    printf "\t<appender name=\"GUAC-DEBUG\" class=\"ch.qos.logback.core.FileAppender\">\n"
+    printf "\t\t<file>/var/log/tomcat6/Guacamole.log</file>\n"
+    printf "\t\t<encoder>\n"
+    printf "\t\t\t<pattern>%%d{HH:mm:ss.SSS} [%%thread] %%-5level %%logger{36} - %%msg%%n</pattern>\n"
+    printf "\t\t</encoder>\n"
+    printf "\t</appender>\n"
+    printf "\n"
+    printf "\t<!-- Log at DEBUG level -->\n"
+    printf "\t<root level=\"debug\">\n"
+    printf "\t\t<appender-ref ref=\"GUAC-DEBUG\"/>\n"
+    printf "\t</root>\n"
+    printf "\n"
+    printf "</configuration>\n"
 ) > /etc/guacamole/logback.xml
 
 
@@ -157,22 +163,22 @@ chcon system_u:object_r:bin_t:s0 /etc/profile.d/guacamole.*
 
 # Add a proxy-directive to Apache
 (
-  printf "<Location /guacamole/>\n"
-  printf "\tOrder allow,deny\n"
-  printf "\tAllow from all\n"
-  printf "\tProxyPass http://localhost:8080/guacamole/"
-  printf " flushpackets=on\n"
-  printf "\tProxyPassReverse http://localhost:8080/guacamole/\n"
-  printf "\tProxyPassReverseCookiePath /guacamole/ /guacamole/\n"
-  printf "</Location>\n"
+    printf "<Location /guacamole/>\n"
+    printf "\tOrder allow,deny\n"
+    printf "\tAllow from all\n"
+    printf "\tProxyPass http://localhost:8080/guacamole/"
+    printf " flushpackets=on\n"
+    printf "\tProxyPassReverse http://localhost:8080/guacamole/\n"
+    printf "\tProxyPassReverseCookiePath /guacamole/ /guacamole/\n"
+    printf "</Location>\n"
 ) > /etc/httpd/conf.d/Guacamole-proxy.conf
 
 # Set redirect Guacamole to /etc/guacamole
 mkdir /usr/share/tomcat6/.guacamole && \
-   cd /usr/share/tomcat6/.guacamole
+    cd /usr/share/tomcat6/.guacamole
 for FILE in /etc/guacamole/*
 do
-   ln -s ${FILE}
+    ln -s ${FILE}
 done
 
 # Start services
