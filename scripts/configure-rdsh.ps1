a0816d63a978a9381df772c08c0c71c7d812b062
Loren Gordon <loren@fleet-it.com>
2015-11-20 15:08:57 -0500

    minor formatting edits

--- scripts/configure-rdsh.ps1
+++ scripts/configure-rdsh.ps1
@@ -6,29 +6,27 @@ Param(
     )
 
 #Based on:
-# * hhttps://s3.amazonaws.com/app-chemistry/scripts/configure-rdsh.ps1
+# * https://s3.amazonaws.com/app-chemistry/scripts/configure-rdsh.ps1
 
 if (-not $ServerFQDN) {
     $name = invoke-restmethod -uri http://169.254.169.254/latest/meta-data/public-hostname
     if (-not $name) {
         $name = [System.Net.DNS]::GetHostByName('').HostName
     }
-    $ServerFQDN = $name 
-} 
-
-$null = Install-WindowsFeature RDS-RD-Server
-$null = Import-Module remotedesktopservices 
-                  
-if ($DomainNetBiosName -and $GroupName) {
-   $group = [ADSI]"WinNT://$env:COMPUTERNAME/Remote Desktop Users,group"
-   $groupmembers = @(@($group.Invoke("Members")) | `
-    foreach {$_.GetType().InvokeMember("Name", 'GetProperty', $null, $_, $null)})
-
-if ($groupmembers -notcontains $GroupName) {
-    group.Add("WinNT://$DomainNetBiosName/$GroupName,group")
-    
-   }
-
+    $ServerFQDN = $name
 }
 
-Powershell -command Restart-Computer
+$null = Install-WindowsFeature RDS-RD-Server
+$null = Import-Module remotedesktopservices
+
+if ($DomainNetBiosName -and $GroupName) {
+    $group = [ADSI]"WinNT://$env:COMPUTERNAME/Remote Desktop Users,group"
+    $groupmembers = @(@($group.Invoke("Members")) | `
+        foreach {$_.GetType().InvokeMember("Name", 'GetProperty', $null, $_, $null)})
+
+    if ($groupmembers -notcontains $GroupName) {
+        group.Add("WinNT://$DomainNetBiosName/$GroupName,group")
+    }
+}
+
+Restart-Computer
