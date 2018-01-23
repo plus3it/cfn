fa5e1c1f2cfe38ec730b029a1a8a330d26e1c496
Loren Gordon <lorengordon@users.noreply.github.com>
2018-01-23 17:56:09 -0500

    Returns more quickly if host is temporarily unavailable

--- scripts/configure-rdsh.ps1
+++ scripts/configure-rdsh.ps1
@@ -216,7 +216,7 @@ try
             Write-Verbose ("Testing connectivity to host: {0}" -f $RDServer.Server)
             try
             {
-                $TestRdp = Retry-TestCommand -Test Test-NetConnection -Args @{ComputerName=$RDServer.Server; CommonTCPPort="RDP"} -TestProperty "TcpTestSucceeded" -Tries 4 -SecondsDelay 45
+                $TestRdp = Retry-TestCommand -Test Test-NetConnection -Args @{ComputerName=$RDServer.Server; CommonTCPPort="RDP"} -TestProperty "TcpTestSucceeded" -Tries 7 -SecondsDelay 17
                 $ValidRDServers += $RDServer.Server
                 Write-Verbose "Successfully connected to host, $($RDServer.Server), keeping this RD Server"
             }
@@ -317,7 +317,7 @@ try
                 # Host is in ACL, but was not an RD Server; test connectivity and remove the rule if the host is not responding
                 try
                 {
-                    $TestRdp = Retry-TestCommand -Test Test-NetConnection -Args @{ComputerName=$Server; CommonTCPPort="RDP"} -TestProperty "TcpTestSucceeded" -Tries 4 -SecondsDelay 45
+                    $TestRdp = Retry-TestCommand -Test Test-NetConnection -Args @{ComputerName=$Server; CommonTCPPort="RDP"} -TestProperty "TcpTestSucceeded" -Tries 7 -SecondsDelay 17
                     Write-Verbose "Successfully connected to host, keeping this access rule"
                     Write-Verbose "    Host: $Server"
                     Write-Verbose ("    Rule Identity: {0}" -f $Rule.IdentityReference.Value)
