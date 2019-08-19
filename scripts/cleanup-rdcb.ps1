f316999c85f75b0b55e9d882aa25fec7a337ecdc
Loren Gordon <lorengordon@users.noreply.github.com>
2019-08-19 15:35:10 -0700

    Provides script to cleanup session hosts from connection broker

---
+++ scripts/cleanup-rdcb.ps1
@@ -0,0 +1,37 @@
+[CmdLetBinding()]
+
+#Requires -Modules RemoteDesktop, P3RemoteAccess, P3Utils
+#Requires -RunAsAdministrator
+
+Param(
+    [Parameter(Mandatory=$true)]
+    [String]
+    $UpdPath,
+
+    [Parameter(Mandatory=$false)]
+    [String]
+    $ConnectionBroker = [System.Net.DNS]::GetHostByName('').HostName,
+
+    [Parameter(Mandatory=$false)]
+    [String]
+    $CollectionName = "RDS Collection"
+)
+$SessionHosts = Get-RDSessionHost -CollectionName $CollectionName -ConnectionBroker $ConnectionBroker -ErrorAction Stop
+$TestedSessionHosts = Test-RetryNetConnection -ComputerName $SessionHosts.SessionHost
+
+if ($TestedSessionHosts.StaleComputers) {
+    Clear-RDSessionHost -SessionHost ($SessionHosts | Where-Object { $_.SessionHost -in $TestedSessionHosts.StaleComputers}) -ConnectionBroker $ConnectionBroker
+}
+
+$Acl = Get-Acl $UpdPath -ErrorAction Stop
+
+$IdentityReferences = @()
+if ($TestedSessionHosts.ValidComputers) {
+    $IdentityReferences = $TestedSessionHosts.ValidComputers | ForEach-Object { "${DomainNetBiosName}\{0}$" -f $_.Split(".")[0] }
+}
+
+$ValidAcl = Edit-AclIdentityReference -Acl $Acl -IdentityReference $IdentityReferences
+
+if ($ValidAcl) {
+    Invoke-RetryCommand -Command Set-Acl -ArgList @{Path=$UpdPath; AclObject=$ValidAcl} -CheckExpression '$?'
+}
