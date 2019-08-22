1d7838f3b46ba3a4441713bbb61d5cadda71704e
Loren Gordon <lorengordon@users.noreply.github.com>
2019-08-22 11:11:12 -0700

    Ensures verbose preference is passed through rdcb cleanup script

--- scripts/cleanup-rdcb.ps1
+++ scripts/cleanup-rdcb.ps1
@@ -17,10 +17,10 @@ Param(
     $CollectionName = "RDS Collection"
 )
 $SessionHosts = Get-RDSessionHost -CollectionName $CollectionName -ConnectionBroker $ConnectionBroker -ErrorAction Stop
-$TestedSessionHosts = Test-RetryNetConnection -ComputerName $SessionHosts.SessionHost
+$TestedSessionHosts = Test-RetryNetConnection -ComputerName $SessionHosts.SessionHost -Verbose:$VerbosePreference
 
 if ($TestedSessionHosts.StaleComputers) {
-    Clear-RDSessionHost -SessionHost ($SessionHosts | Where-Object { $_.SessionHost -in $TestedSessionHosts.StaleComputers}) -ConnectionBroker $ConnectionBroker
+    Clear-RDSessionHost -SessionHost ($SessionHosts | Where-Object { $_.SessionHost -in $TestedSessionHosts.StaleComputers}) -ConnectionBroker $ConnectionBroker -Verbose:$VerbosePreference
 }
 
 $Acl = Get-Acl $UpdPath -ErrorAction Stop
@@ -30,8 +30,8 @@ if ($TestedSessionHosts.ValidComputers) {
     $IdentityReferences = $TestedSessionHosts.ValidComputers | ForEach-Object { "${DomainNetBiosName}\{0}$" -f $_.Split(".")[0] }
 }
 
-$ValidAcl = Edit-AclIdentityReference -Acl $Acl -IdentityReference $IdentityReferences
+$ValidAcl = Edit-AclIdentityReference -Acl $Acl -IdentityReference $IdentityReferences -Verbose:$VerbosePreference
 
 if ($ValidAcl) {
-    Invoke-RetryCommand -Command Set-Acl -ArgList @{Path=$UpdPath; AclObject=$ValidAcl} -CheckExpression '$?'
+    Invoke-RetryCommand -Command Set-Acl -ArgList @{Path=$UpdPath; AclObject=$ValidAcl} -CheckExpression '$?' -Verbose:$VerbosePreference
 }
